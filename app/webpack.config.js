2f9d9408536b54c52cd3b7d2cd3fb5911e585e40
Siddartha Reddy <reddy.siddartha53@gmail.com>
2020-07-08 12:41:56 -0700

    contract for uploading product

---
+++ app/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        loader: 'css-loader',
+        options: {
+          url: true,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+  ],
+  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+};
